e7d94dc75eb71f15bd3b27d3579f97ac11eb0ae1
eddiemosh <hardyedward18@gmail.com>
2024-08-18 21:33:15 +0100

    Adding header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIcon} from "@angular/material/icon";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatIcon, MatButton, MatIconButton, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
