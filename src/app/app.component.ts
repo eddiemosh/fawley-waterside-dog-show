ab28cedd297320c748ac43df5c8f2da0e4e43b1e
eddiemosh <hardyedward18@gmail.com>
2024-08-22 23:47:48 +0100

    Adding slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import {MatToolbar} from "@angular/material/toolbar";
 import {MatIcon} from "@angular/material/icon";
 import {MatButton, MatIconButton} from "@angular/material/button";
 import {HeaderComponent} from "./header/header.component";
+import {SlideshowComponent} from "./slideshow/slideshow.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbar, MatIcon, MatButton, MatIconButton, HeaderComponent],
+  imports: [RouterOutlet, MatToolbar, MatIcon, MatButton, MatIconButton, HeaderComponent, SlideshowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
