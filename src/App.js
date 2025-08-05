65f9cb2efbebebc2a97b900617fbd60dc5446e29
eddiemosh <hardyedward18@gmail.com>
2025-08-05 17:40:38 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ import Events from './components/events/events';
 import TicketSelection from './components/tickets/ticket-selection';
 import Payments from './components/payments/payments';
 import PaymentOptions from "./components/payments/payment-options/payment-options";
-import {Elements} from "@stripe/react-stripe-js";
-import {loadStripe} from "@stripe/stripe-js";
 
-// Load your Stripe public key here (Test or Live)
-const stripePromise = loadStripe('pk_test_51RsBRuCYSxVmD9YE961zzGoiktGTR3mBTNE5uMUF5uKPbkh3yC7lmDJJjWfPlb8ijNdNEk32xSTe0dlNnvmQ3MYJ00huCxokEG')
 function App() {
     return (
         <Router>
@@ -22,9 +18,7 @@ function App() {
                 <Route path="/tickets" element={<TicketSelection/>}/>
                 <Route path="/payments" element={<Payments/>}/>
                 <Route path="/payment-options" element={
-                    <Elements stripe={stripePromise}>
-                        <PaymentOptions/>
-                    </Elements>
+                    <PaymentOptions/>
                 }/>
             </Routes>
         </Router>
